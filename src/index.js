85dcbff58998e12e8007a67704497486c11a62dc
Soyun <yun@YunBook.local>
2023-04-04 05:54:15 +0900

    Second Commit - Nursery Manager

--- src/index.js
+++ src/index.js
@@ -2,13 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
+import { HelmetProvider } from 'react-helmet-async';
 import reportWebVitals from './reportWebVitals';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if(!user) return;
+
+    store.dispatch(tempSetUser(JSON.parse(user)));
+    store.dispatch(check());
+  } catch(e) {
+    console.log('localStroage is not working');
+  }
+}
+
+sagaMiddleware.run(rootSaga);
+loadUser();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import HeaderContainer from '../components/common/HeaderContainer';
+import MainContainer from '../containers/main/MainContainer';
+
+const MainPage = () => {
+    return(
+        <>
+            <HeaderContainer />
+            <MainContainer />
+        </>
+    );
+};
+
+export default MainPage;
